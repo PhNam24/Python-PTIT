4c1e425995442704ae2034eb42b47d2ee5d10fdd
Phạm Hoài Nam <98546157+PhNam24@users.noreply.github.com>
2023-08-22 13:37:18 +0700

    src

--- BT/PY01003 - Lam tron so.py
+++ BT/PY01003 - Lam tron so.py
@@ -11,7 +11,8 @@ while t:
             n //= 10
         arr.reverse()
         for i in range(len(arr) - 1, 0, -1):
-            if arr[i] >= 5: arr[i - 1] += 1
+            if arr[i] >= 5: 
+                arr[i - 1] += 1
             arr[i] = 0
         for i in arr: print(i, end = "", sep = "")
     print("\n")
